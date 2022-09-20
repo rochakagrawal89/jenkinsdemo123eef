319f14cfb1265a4a7b0b8043311a71885e99d2a5
Cloud User <azureuser@jenkins-practise.j1mpnsletake1lnnvc5tyakexf.bx.internal.cloudapp.net>
2022-09-20 02:51:55 +0000

    commited from the local system

--- demo.sh
+++ demo.sh
@@ -1,6 +1,1 @@
-echo "I have done some changes and they should reflect in jenkins build"
-
-echo "I have added a test environment and it should trigger after dev"
-
-
-echo "I have applied those changes in github webhook:"
+echo "I have made these changes from the local system"
